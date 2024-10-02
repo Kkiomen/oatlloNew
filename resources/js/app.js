50198697669ccf85cf0df63a2de52a333aefbbd3
Jakub Owsianka <j.owsianka@sente.pl>
2024-10-02 21:34:05 +0200

    fix js

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,7 +13,7 @@ Alpine.start();
 
 
 
-const url = 'https://temp-dashboard.oatllo.pl/';
+const url = 'https://temp-dashboard.oatllo.pl';
 const urlSegments = window.location.pathname.split('/');
 const pageId = urlSegments[2]; //
 
