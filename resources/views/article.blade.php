6cb298a712cdd7c99892c428e6046ece9d515927
Jakub Owsianka <j.owsianka@sente.pl>
2024-10-08 22:28:09 +0200

    Add views

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -112,7 +112,7 @@
                             </ol>
                         </nav>
 
-                        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
+                        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Naprawa Falowników Fotowoltaicznych Fronius</h1>
                         <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
                         <div class="mt-10 flex items-center gap-x-6">
                             <a href="{{ route('index') }}#contact" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Skorzystaj z usług</a>
