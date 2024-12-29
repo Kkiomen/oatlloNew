c427314e1dd428f0b05d80a2c73f1689310f6591
Jakub Owsianka <j.owsianka@sente.pl>
2024-12-29 19:20:55 +0100

    Fix Sitemap

--- app/Services/HomeService.php
+++ app/Services/HomeService.php
@@ -28,7 +28,17 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N9MC7J53');</script>
-<!-- End Google Tag Manager -->";
+<!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-D4D0GSS6WQ\"></script>
+<script>
+            window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D4D0GSS6WQ');
+</script>
+";
         }
         return '';
     }
