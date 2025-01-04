b446d9a22db6fb13eaee09d217080ad77dc1b584
Jakub Owsianka <j.owsianka@sente.pl>
2025-01-04 17:03:02 +0100

    Hide in random articles... lessons from course

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,11 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Tag;
-use App\Prompts\Abstract\Enums\OpenApiResultType;
-use App\Prompts\GenerateArticleBasicInformationToOtherLanguagePrompt;
-use App\Prompts\GenerateArticleContentToOtherLanguagePrompt;
-use App\Services\Article\ArticleService;
-use App\Services\Generator\GeneratorArticleService;
 use App\Services\Generator\TagForArticleGenerator;
 use App\Services\Helper\LanguageHelper;
 use App\Services\ImageService;
@@ -23,14 +18,13 @@ class TestController extends Controller
     const URL_POLIGON_VERIFY = 'https://poligon.aidevs.pl/verify';
 
 
-    public function test(Request $request, SitemapService $sitemapService)
+    public function test(Request $request)
     {
         $tags = Tag::where('title_seo', null)->inRandomOrder()->get();
 
         foreach ($tags as $tag) {
             TagForArticleGenerator::createSeoInformation($tag);
         }
-//        $sitemapService->generateSitemap();
     }
 
 }
