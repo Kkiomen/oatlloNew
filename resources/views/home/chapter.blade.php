23bb9b665dd2cf55137a74348be43fe84d1314d8
Jakub Owsianka <j.owsianka@wiseb2b.eu>
2025-09-06 13:52:52 +0200

    Add course php

--- resources/views/home/chapter.blade.php
+++ resources/views/home/chapter.blade.php
@@ -346,8 +346,8 @@
     {
       "@context": "https://schema.org",
       "@type": "Course",
-      "name": {!! json_encode($courseCategory->title) !!},
-      "description": {!! json_encode($courseCategory->description) !!},
+      "name": {{ json_encode($courseCategory->title ?: 'Course Chapter') }},
+      "description": {{ json_encode($courseCategory->description ?: 'Learn programming with this course chapter') }},
       "url": "{{ $courseCategory->getRoute() }}",
       "provider": {
         "@type": "Organization",
@@ -359,7 +359,7 @@
       "inLanguage": "{{ env('APP_LANG_HTML') }}",
       "isPartOf": {
         "@type": "Course",
-        "name": {!! json_encode($course->title_list) !!},
+        "name": {{ json_encode($course->title_list) }},
         "url": "{{ $course->getRoute() }}"
       },
       "hasCourseInstance": {
@@ -404,13 +404,13 @@
         {
           "@type": "ListItem",
           "position": 3,
-          "name": {!! json_encode($course->title_list) !!},
+          "name": {{ json_encode($course->title_list) }},
           "item": "{{ $course->getRoute() }}"
         },
         {
           "@type": "ListItem",
           "position": 4,
-          "name": {!! json_encode($courseCategory->title) !!},
+          "name": {{ json_encode($courseCategory->title) }},
           "item": "{{ $courseCategory->getRoute() }}"
         }
       ]
