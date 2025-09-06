a02aa9117ada09443aa9172e3e1aa764aff512b3
Jakub Owsianka <j.owsianka@wiseb2b.eu>
2025-09-06 13:58:09 +0200

    Add course php

--- resources/views/home/chapter.blade.php
+++ resources/views/home/chapter.blade.php
@@ -347,7 +347,7 @@
       "@context": "https://schema.org",
       "@type": "Course",
       "name": {!! json_encode($courseCategory->title, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!},
-      "description": "{!! json_encode($courseCategory->description, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}",
+      "description": {!! json_encode($courseCategory->description, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!},
       "url": "{{ $courseCategory->getRoute() }}",
       "provider": {
         "@type": "Organization",
@@ -404,13 +404,13 @@
         {
           "@type": "ListItem",
           "position": 3,
-          "name": "{!! json_encode($course->title_list, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}",
+          "name": {!! json_encode($course->title_list, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!},
           "item": "{{ $course->getRoute() }}"
         },
         {
           "@type": "ListItem",
           "position": 4,
-          "name": "{!! json_encode($courseCategory->title, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}",
+          "name": {!! json_encode($courseCategory->title, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!},
           "item": "{{ $courseCategory->getRoute() }}"
         }
       ]
