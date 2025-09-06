656cf7399edab0822515d184908328e40f9d6e43
Jakub Owsianka <j.owsianka@wiseb2b.eu>
2025-09-06 13:55:34 +0200

    Add course php

--- resources/views/home/chapter.blade.php
+++ resources/views/home/chapter.blade.php
@@ -347,7 +347,7 @@
       "@context": "https://schema.org",
       "@type": "Course",
       "name": {!! json_encode($courseCategory->title, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!},
-      "description": {{ json_encode($courseCategory->description ?: 'Learn programming with this course chapter') }},
+      "description": {!! json_encode($courseCategory->description, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!},
       "url": "{{ $courseCategory->getRoute() }}",
       "provider": {
         "@type": "Organization",
